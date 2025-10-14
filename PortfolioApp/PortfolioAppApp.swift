a9f5fb0c38130778e1d2ba06b0e05ea277cf973f
Jaloliddin Zokirov <jaloliddinzokirov.dev@gmail.com>
2025-10-15 04:03:09 +0500

    Initial Commit

---
+++ PortfolioApp/PortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppApp.swift
+//  PortfolioApp
+//
+//  Created by Jaloliddin Zokirov on 15/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
